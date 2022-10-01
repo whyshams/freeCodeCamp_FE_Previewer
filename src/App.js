da1d371a8b70719c7a5d30c5b13ee5a29d4969d6
Nuren Shams Chowdhury <chowdhury.noren@gmail.com>
2022-10-01 23:02:02 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Markdown from 'marked-react';
 
-function App() {
+const App = () => {
+  const [text,setText] = useState(`
+  ![Dwinatech Logo](https://yt3.ggpht.com/ytc/AKedOLRCB8l9dwq0qVxZIb7C7b1G5lvYlFIYzymD1Dkx=s176-c-k-c0x00ffffff-no-rj)
+  
+  # Hello, 
+  ## You'r welcom at
+  ### DwinaTech Channel
+  
+  
+  \`<div>Inline code</div>\`
+  
+  \`\`\`
+  const multipleLineCode = (param) => {
+      if(param) {
+          return param
+      }
+  }
+  \`\`\`
+  
+  **Some bold text**
+  
+  [Visit My Channel](https://www.youtube.com/channel/UCSS0kFyF7KWjE19Rj9PgNQA)
+  
+  > Block Quot
+  
+  1. First list item
+  2. Second list item
+  `)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <div>
+      <textarea onChange={(e) => setText(e.target.value)} id="editor" >{text}</textarea>
+
+     </div>
+     <div id="preview">
+      <Markdown>{text}</Markdown>
+
+     </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
